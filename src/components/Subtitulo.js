5223cc689dde818f9bc1e310e7baf583789b7f95
Emilse <emilsearias@gmail.com>
2021-03-09 11:33:34 -0300

    agregamos dos componentes

---
+++ src/components/Subtitulo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Subtitulo = () => {
+    return (
+        <div>
+            <h3 className='text-center text-light'>Tareas de la comision</h3>
+        </div>
+    );
+};
+
+// let Subtitulo = function (){
+// }
+
+export default Subtitulo;
---
+++ src/components/Titulo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Titulo extends Component {
+    render() {
+        return (
+            <div>
+               <h1 className='text-center text-light display-4 my-4'>Lista de Tareas</h1>
+            </div>
+        );
+    }
+}
+
+export default Titulo;
