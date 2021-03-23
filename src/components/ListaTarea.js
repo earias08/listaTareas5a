0aaa54aa0043e553dc671bdb6dd7dfb81f9186d0
Emilse <emilsearias@gmail.com>
2021-03-23 10:40:52 -0300

    se agrego la logica para mostrar las tareas en el componente ListaTarea

--- src/components/ListaTarea.js
+++ src/components/ListaTarea.js
@@ -1,18 +1,17 @@
 import React from "react";
+import TareaIndividual from "./TareaIndividual";
 
 const ListaTarea = (props) => {
   // codigo de javascript
-  console.log(props.tareas);
+  // console.log(props.tareas);
 
   return (
     //   aqui puedo seguir con js
     <ul className="list-group">
       {/* aqui ya es html */}
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
+      {props.tareas.map((dato, indice) => (
+        <TareaIndividual nombreTarea={dato} key={indice}></TareaIndividual>
+      ))}
     </ul>
   );
 };
--- src/components/TareaIndividual.js
+++ src/components/TareaIndividual.js
@@ -1,10 +1,8 @@
 import React from 'react';
 
-const TareaIndividual = () => {
+const TareaIndividual = (props) => {
     return (
-        <div>
-            
-        </div>
+        <li className="list-group-item">{props.nombreTarea}</li>
     );
 };
 
